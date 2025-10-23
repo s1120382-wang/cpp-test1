c7cb867c04c8fed1d02c544cc965c6ebdc4bb95e
s1120382-wang <s1120382@dmhs.kh.edu.tw>
2025-10-23 14:05:13 +0800

    作業6

--- main.cpp
+++ main.cpp
@@ -1,30 +1,32 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
-    // --- Part 1: 九九乘法表 ---
-    cout << "九九乘法表：" << endl;
-    int i = 1;
-    while (i <= 9) {
-        int j = 1;
-        while (j <= 9) {
-            cout << i << " * " << j << " = " << i * j << "\t";
-            j += 1;
+    int score[10] = {85, 90, 60, 92, 100, 76, 50, 89, 84, 40};  // 學生成績
+    int gradeCount[5] = {0};  // 用來記錄各個等級的人數
+    
+    // 迴圈檢查每一個學生的成績
+    for (int i = 0; i < 10; ++i) {
+        if (score[i] >= 90) {
+            gradeCount[0]++;  // A 等級
+        } else if (score[i] >= 80) {
+            gradeCount[1]++;  // B 等級
+        } else if (score[i] >= 70) {
+            gradeCount[2]++;  // C 等級
+        } else if (score[i] >= 60) {
+            gradeCount[3]++;  // D 等級
+        } else {
+            gradeCount[4]++;  // F 等級
         }
-        cout << endl;
-        i += 1;
     }
 
-    cout << endl;
-
-    // --- Part 2: 星號金字塔 ---
-    cout << "星號金字塔：" << endl;
-    for (int i = 1; i <= 6; ++i) {
-        for (int j = 1; j <= i; ++j) {
-            cout << "*";
-        }
-        cout << endl;
-    }
+    // 顯示統計結果
+    cout << "A 等人數: " << gradeCount[0] << endl;
+    cout << "B 等人數: " << gradeCount[1] << endl;
+    cout << "C 等人數: " << gradeCount[2] << endl;
+    cout << "D 等人數: " << gradeCount[3] << endl;
+    cout << "F 等人數: " << gradeCount[4] << endl;
 
     return 0;
 }
